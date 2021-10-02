ae8ec9fce02f58fa6f155ab51673fce595030691
27433 <274337383@com>
2021-10-03 04:58:45 +0800

    hello

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>主页</title>
+    <link rel="stylesheet" href="CSS/layout.css">
+</head>
+<body>
+<div>
+    <h1 class="title">主页（Beta）</h1>
+</div>
+<hr />
+<div>
+    <a href="https://www.baidu.com">百度一下</a>
+    <a href="homework/saint.html">圣诞的由来</a>
+    <a href="PHP_Page/index.php">体育新闻</a>
+    <a href="homework/CSSlast.html">linux新闻</a>
+    <a href="homework/MI_test.html">小米侧边栏</a>
+</div>
+<hr />
+
+<div>
+    <h3>下载</h3>
+    <br />
+    <a href="http://b.downya.com/200/photoshopcs6_itmop.com.zip">PhotoShop CS6下载</a>
+</div>
+
+<br />
+<div>
+    <?php
+        echo PHP_VERSION;
+    ?>
+</div>
+</body>
+</html>
